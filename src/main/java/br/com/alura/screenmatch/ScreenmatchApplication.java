14c3aa5b0d8f0deedde09a7c6ebdebebed1e6f0a
Marcos Portela <marcosnasp@gmail.com>
2024-06-26 08:38:53 -0300

    Adicionando busca por dados de temporada

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,5 +1,8 @@
 package br.com.alura.screenmatch;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,7 +15,9 @@ import org.springframework.context.annotation.Primary;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import br.com.alura.screenmatch.model.DadosEpisodios;
 import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.IConversorDados;
 
@@ -26,10 +31,14 @@ public class ScreenmatchApplication {
 	private final ConsumoApi consumoApi;
 
 	private final IConversorDados<DadosSerie> conversorDados;
+	private final IConversorDados<DadosEpisodios> conversorDadosEpisodios;
+	private final IConversorDados<DadosTemporada> conversorDadosTemporada;
 
-	public ScreenmatchApplication(ConsumoApi consumoApi, @Lazy IConversorDados<DadosSerie> conversorDados) {
+	public ScreenmatchApplication(ConsumoApi consumoApi, @Lazy IConversorDados<DadosSerie> conversorDados, @Lazy IConversorDados<DadosEpisodios> conversorDadosEpisodios, @Lazy IConversorDados<DadosTemporada> conversorDadosTemporada) {
 		this.consumoApi = consumoApi;
 		this.conversorDados = conversorDados;
+		this.conversorDadosEpisodios = conversorDadosEpisodios;
+		this.conversorDadosTemporada = conversorDadosTemporada;
 	}
 
 	public static void main(String[] args) {
@@ -49,14 +58,33 @@ public class ScreenmatchApplication {
 
     @Bean
     CommandLineRunner consumirDados() {
-		String endereco = String.format("https://www.omdbapi.com/?t=gilmore+girls&apikey=%s", apiKey);
+		String serie = "gilmore girls".replaceAll(" ", "%20");
+		String endereco = String.format("https://www.omdbapi.com/?t=%s&apikey=%s", serie, apiKey);
 		
 		LOGGER.info("Requisitando: {}", endereco);
 		return args -> {
+			List<DadosTemporada> listaDadosTemporada = new ArrayList<>();
 			String dados = consumoApi.obterDados(endereco);
 			LOGGER.info("Resposta via CommandLineRunner: {}", dados);
 			DadosSerie dadosSerie = conversorDados.obterDados(dados, DadosSerie.class);
+
+			String dadosEpisodios = consumoApi.obterDados(String.format("https://www.omdbapi.com/?t=%s&season=1&episode=2&apikey=%s", serie, apiKey));
+			LOGGER.info("Requisitando dados de episodios: {}", dadosEpisodios);
+			DadosEpisodios dadosEpisodiosObjeto = conversorDadosEpisodios.obterDados(dadosEpisodios, DadosEpisodios.class);
+			
 			LOGGER.info("Dados da Serie: \n Title: {}, \nAvaliacao:  {}, \nTotal de Temporadas: {}", dadosSerie.title(), dadosSerie.avaliacao(), dadosSerie.totalTemporadas());
+			LOGGER.info("Dados Episodios: \n Title: {}, \nNumero:  {}, \n Avaliacao: {}, \nLançamento: {}", dadosEpisodiosObjeto.titulo(), dadosEpisodiosObjeto.numero(), dadosEpisodiosObjeto.avaliacao(), dadosEpisodiosObjeto.dataLancamento());
+			
+			for (int temporada = 1; temporada <= dadosSerie.totalTemporadas(); temporada++) {
+				String dadosTemporada = consumoApi.obterDados(String.format("https://www.omdbapi.com/?t=%s&season=%d&apikey=%s", serie, temporada, apiKey));
+				LOGGER.info("Requisitando dados da temporada: {}", dadosTemporada);
+				DadosTemporada dadosTemporadaObjeto = conversorDadosTemporada.obterDados(dadosTemporada, DadosTemporada.class);
+				LOGGER.info("Dados Temporada: \nNumero:  {}, \n Episodios: {}", dadosTemporadaObjeto.numeroTemporada(), dadosTemporadaObjeto.dadosEpisodios());
+
+				listaDadosTemporada.add(dadosTemporadaObjeto);
+			}
+
+			listaDadosTemporada.forEach(t -> LOGGER.info(t.toString()));
 		};
 	} 
 
